a34d12cdc1f28552850fe6798ffc2deb8aa342c3
theonewhoknew <theonewhoknew@protonmail.com>
2023-12-05 10:38:52 +0100

    update

--- ex03/src/Intern.cpp
+++ ex03/src/Intern.cpp
@@ -1,28 +1,21 @@
-AForm::AForm(): _name("default"), _signed(false), _sign_grade(150), _exec_grade(150)
+#include "../inc/Intern.hpp"
+
+Intern::Intern()
 {	
 
 }
 
-AForm::AForm(const std::string name, const int sign, const int exec) : _name(name), _signed(false), _sign_grade(sign), _exec_grade(exec)
-{
-	if (this->getSignGrade() < 1 || this->getExecGrade() < 1)
-		throw AForm::GradeTooHighException();
-	else if (this->getSignGrade() > 150 || this->getExecGrade() > 150)
-		throw AForm::GradeTooLowException();
-}
-
-AForm::AForm(AForm &copy) : _name(copy.getName()), _signed(copy.getSigned()), _sign_grade(copy.getSignGrade()), _exec_grade(copy.getExecGrade()) 
+Intern::Intern(Intern &copy)
 {	
 
 }
 
-AForm& AForm::operator=(const AForm &instance)
+Intern& Intern::operator=(const Intern &instance)
 {
-	_signed = instance.getSigned();
 	return (*this);
 }
 
-AForm::~AForm()
+Intern::~Intern()
 {
 
 }
